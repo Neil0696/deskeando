3fc9952f0cabf4dacbf807c5063a77ff2eac8c62
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2022-01-20 12:43:43 +0000

    Create a new submit form as booking screen
    access the page with path: /booking

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,15 @@ import { Route, Switch } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
+import BookingScreen from "./pages/BookingScreen";
 
 const App = () => (
 	<Switch>
 		<Route path="/" exact>
 			<Home />
+		</Route>
+		<Route path="/booking">
+			<BookingScreen />
 		</Route>
 		<Route path="/about/this/site">
 			<About />
