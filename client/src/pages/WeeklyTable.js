85c440c5d3e2c97e1a8359c24adbe3229ffeadd6
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2022-01-20 17:29:11 +0000

    filter sample data to the weekly table

--- client/src/pages/WeeklyTable.js
+++ client/src/pages/WeeklyTable.js
@@ -2,21 +2,26 @@ import React from "react";
 
 import TableHeading from "./TableHeading";
 
-const WeeklyTable = (props) => {
-	return (
-		<div>
-			<table className="table">
-				<TableHeading />
-				<tbody>
-					{props.result.map((info, index) => (
-						<tr key={index}>
-							<td>{info.username}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
-	);
-}
+const fillInRow = (seat, bookings) =>
+	bookings
+		.filter((booking) => booking.desk_user_id === seat)
+		.map((b, index) => (
+			<td key={index}>
+				{b.username}
+			</td>
+		));
+
+const WeeklyTable = (props) => (
+	<div>
+		<table>
+			<TableHeading />
+			<tbody>
+				<tr>{fillInRow(1, props.result)}</tr>
+				<tr>{fillInRow(2, props.result)}</tr>
+				<tr>{fillInRow(3, props.result)}</tr>
+			</tbody>
+		</table>
+	</div>
+);
 
 export default WeeklyTable;
