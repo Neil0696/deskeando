5141f6e8304c2f6318a21451a9987a7b306c0c55
Rebeca Lopez-Santos <rebecal.santos@hotmail.com>
2022-01-20 22:34:11 +0000

    new booking endpoint
    The format for data return from getbookings have been changed. these changes create tables.sql so you need to run the sql again.

--- server/api.js
+++ server/api.js
@@ -7,13 +7,42 @@ router.get("/", (req, res) => {
 	res.json({ message: "Hello, world!" });
 });
 
-router.get("/bookings", (req, res) => {
-	db.query("SELECT * from booking INNER JOIN desk_user ON booking.desk_user_id=desk_user.id;")
-    .then((result) => res.json(result.rows))
-	.catch((e) => {
+router.get("/bookings", async (req, res) => {
+	try {
+		const result = await db.query("SELECT booking.*,desk_user.username from booking INNER JOIN desk_user ON booking.desk_user_id=desk_user.id;"
+		);
+		// convert array with objects having {username, booking_date} into {name,date}
+		const bookings = result.rows.map((row) => {
+			return { id: row.id, name: row.username, date: row.booking_date };
+		});
+		res.json(bookings);
+	} catch (e) {
 		console.error(e);
 		res.sendStatus(400);
-	});
+	}
+});
+
+router.post("/bookings", async function (req, res) {
+	const userName = req.body.name;
+	const bookingDate = req.body.date;
+
+	const insertQuery =
+		"INSERT INTO booking (desk_user_id, booking_date) VALUES ($1, $2)";
+	try {
+		const userResult = await db.query(
+			"SELECT id from desk_user WHERE username=$1",
+			[userName]
+		);
+		if (userResult.rows.length === 0) {
+			return res.status(400).send(`User with name ${userName} does not exist`);
+		}
+		const userId = userResult.rows[0].id;
+		const bookingResult = await db.query(insertQuery, [userId, bookingDate]);
+		res.send("Booking created!");
+	} catch (e) {
+		console.error(e);
+		res.sendStatus(400);
+	}
 });
 
 export default router;
--- server/create_tables.sql
+++ server/create_tables.sql
@@ -6,13 +6,13 @@ create table booking (
     desk_user_id INT REFERENCES desk_user(id),
     booking_date DATE
 );
-INSERT INTO desk_user (id, username) VALUES (1, 'Maha'); 
-INSERT INTO desk_user (id, username) VALUES (2, 'Sam'); 
-INSERT INTO desk_user (id, username) VALUES (3, 'Pablo'); 
-INSERT INTO booking (id, desk_user_id, booking_date) VALUES (1, 1, '2022-01-17'); 
-INSERT INTO booking (id, desk_user_id, booking_date) VALUES (2, 2, '2022-01-19');
-INSERT INTO booking (id, desk_user_id, booking_date) VALUES (3, 1, '2022-01-19');
-INSERT INTO booking (id, desk_user_id, booking_date) VALUES (4, 3, '2022-01-19');
-INSERT INTO booking (id, desk_user_id, booking_date) VALUES (5, 3, '2022-01-20');
-INSERT INTO booking (id, desk_user_id, booking_date) VALUES (6, 1, '2022-01-20');
-INSERT INTO booking (id, desk_user_id, booking_date) VALUES (7, 1, '2022-01-21');
+INSERT INTO desk_user (username) VALUES ('Maha'); 
+INSERT INTO desk_user (username) VALUES ('Sam'); 
+INSERT INTO desk_user (username) VALUES ('Pablo'); 
+INSERT INTO booking (desk_user_id, booking_date) VALUES (1, '2022-01-17'); 
+INSERT INTO booking (desk_user_id, booking_date) VALUES (2, '2022-01-19');
+INSERT INTO booking (desk_user_id, booking_date) VALUES (1, '2022-01-19');
+INSERT INTO booking (desk_user_id, booking_date) VALUES (3, '2022-01-19');
+INSERT INTO booking (desk_user_id, booking_date) VALUES (3, '2022-01-20');
+INSERT INTO booking (desk_user_id, booking_date) VALUES (1, '2022-01-20');
+INSERT INTO booking (desk_user_id, booking_date) VALUES (1, '2022-01-21');
