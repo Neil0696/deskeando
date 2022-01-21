0926d2458d65f6fab9f07bc4b1f024f7f703cf2e
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2022-01-21 17:13:33 +0000

    display bookings in the table

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,42 +1,14 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-// import UsersData from "../Data/UsersData";
+import UsersData from "../Data/UsersData";
 import WeeklyTable from "./WeeklyTable";
 
 import "./Home.css";
 import logo from "./logo.svg";
 
+const ROWS_COUNT = 3;
+
 export function Home() {
-	const [message, setMessage] = useState("Loading...");
-
-	const [result, setResult] = useState([]);
-
-	useEffect(() => {
-		fetch("/api")
-			.then((res) => {
-				if (!res.ok) {
-					throw new Error(res.statusText);
-				}
-				return res.json();
-			})
-			.then((body) => {
-				setMessage(body.message);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}, []);
-
-	useEffect(() => {
-		fetch("/api/bookings")
-			.then((response) => response.json())
-			.then((data) => {
-				// console.log(data, "DATA");
-				setResult(data);
-			})
-			.catch((err) => console.log(err));
-	}, []);
 
 	return (
 		<main role="main">
@@ -47,12 +19,10 @@ export function Home() {
 					src={logo}
 					alt="Just the React logo"
 				/>
-				<h1 className="message" data-qa="message">
-					{message}
-				</h1>
+
 				<Link to="/about/this/site">About</Link>
 			</div>
-			<WeeklyTable bookings={result}/>
+			<WeeklyTable bookings={UsersData} rowsCount={ROWS_COUNT} />
   </main>
 	);
 }
--- client/src/pages/WeeklyTable.js
+++ client/src/pages/WeeklyTable.js
@@ -1,34 +1,53 @@
 import React from "react";
 
+import "./WeeklyTable.css";
 
-const fillInRow = (seat, bookings) =>
-	bookings
-		.filter((booking) => booking.desk_user_id === seat)
-		.map((b, index) => (
-			<td key={index}>
-				{b.username}
-			</td>
-		));
+const week = [
+	"2022-01-17T00:00:00.000Z",
+	"2022-01-18T00:00:00.000Z",
+	"2022-01-19T00:00:00.000Z",
+	"2022-01-20T00:00:00.000Z",
+	"2022-01-21T00:00:00.000Z",
+];
 
-const WeeklyTable = (props) => (
-	<div>
-		<table>
-			<thead>
-				<tr>
-					<th scope="col">Monday Jan 17th</th>
-					<th scope="col">Tue Jan 18th</th>
-					<th scope="col">Wed Jan 19th</th>
-					<th scope="col">Thu Jan 20th</th>
-					<th scope="col">Fri Jan 21st</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>{fillInRow(1, props.bookings)}</tr>
-				<tr>{fillInRow(2, props.bookings)}</tr>
-				<tr>{fillInRow(3, props.bookings)}</tr>
-			</tbody>
-		</table>
-	</div>
-);
+const getBookingsByRow = (bookings, rowsCount, week) => {
+	const bookingsByDay = {};
+	week.forEach((day) => {
+		bookingsByDay[day] = bookings.filter((booking) => booking.date === day);
+	});
+
+	const bookingsByRow = [];
+	for (let i = 0; i < rowsCount; i++) {
+		bookingsByRow[i] = week.map((day) => bookingsByDay[day][i]);
+	}
+	return bookingsByRow;
+};
+
+const WeeklyTable = ({ bookings, rowsCount }) => {
+	const bookingsByRow = getBookingsByRow(bookings, rowsCount, week);
+
+	return (
+		<div>
+			<table>
+				<thead>
+					<tr>
+						{week.map((date) => (
+							<th key={date}>{new Date(date).toLocaleDateString()}</th>
+						))}
+					</tr>
+				</thead>
+				<tbody>
+					{bookingsByRow.map((row, i) => (
+						<tr key={i}>
+							{row.map((e, index) => (
+								<td key={index}>{e?.name}</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
 
 export default WeeklyTable;
