b4312edddd4e9077a0c51d09578254582539a3c9
Benjamin-GitHub <75631604+Benjamin-GitHub@users.noreply.github.com>
2022-01-22 00:56:15 +0000

    Revert "Create a new submit form as booking screen"

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,11 @@ import { Route, Switch } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
-import BookingScreen from "./pages/BookingScreen";
 
 const App = () => (
 	<Switch>
 		<Route path="/" exact>
 			<Home />
-		</Route>
-		<Route path="/booking">
-			<BookingScreen />
 		</Route>
 		<Route path="/about/this/site">
 			<About />
