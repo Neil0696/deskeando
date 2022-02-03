20a9d09c862701cc1164308fc8d5bb2e846b5126
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2022-02-03 00:09:39 +0000

    correct Island and desk's transform (position)

--- client/src/pages/FloorPlan.js
+++ client/src/pages/FloorPlan.js
@@ -1,121 +1,116 @@
 import React from "react";
 
+
 const FloorPlan = () => {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
-			width="329"
-			height="266"
-			viewBox="0 0 329 266"
+			width="848"
+			height="875"
+			viewBox="0 0 848 875"
 		>
 			<rect
-				style={{ fill: "#fff", stroke: "#707070", strokeWidth: "1" }}
+				style={{ fill: "#fff", stroke: "#000000", strokeWidth: "1" }}
 				id="Rect7"
-				width="328"
-				height="265"
+				width="847"
+				height="874"
 				x="0.5"
 				y="0.5"
 			/>
-			<g id="Group1" transform="translate(-81 -93)">
+			<g id="Group1" transform="translate(262 100)">
 				<rect
-					style={{ fill: "#fff", stroke: "#707070", strokeWidth: "1" }}
+					style={{ fill: "#fff", stroke: "#000000", strokeWidth: "1" }}
 					id="Rect1"
-					transform="translate(187 139)"
-					width="52"
-					height="50"
+					transform="translate(0 0)"
+					width="150"
+					height="150"
 					x="0.5"
 					y="0.5"
 				/>
 				<text
 					id="txt1"
-					transform="translate(208 171)"
-					fontSize="20"
-					fontFamily="SegoeUI, Segoe UI"
+					transform="translate(64 90)"
+					fontSize="40"
 				>
 					1
 				</text>
 				<rect
-					style={{ fill: "#fff", stroke: "#707070", strokeWidth: "1" }}
+					style={{ fill: "#fff", stroke: "#000000", strokeWidth: "1" }}
 					id="Rect2"
-					transform="translate(250 139)"
-					width="52"
-					height="50"
+					transform="translate(175 0)"
+					width="150"
+					height="150"
 					x="0.5"
 					y="0.5"
 				/>
 				<text
 					id="txt2"
-					transform="translate(273 171)"
-					fontSize="20"
-					fontFamily="SegoeUI, Segoe UI"
+					transform="translate(239 90)"
+					fontSize="40"
 				>
 					2
 				</text>
 				<rect
-					style={{ fill: "#fff", stroke: "#707070", strokeWidth: "1" }}
+					style={{ fill: "#fff", stroke: "#000000", strokeWidth: "1" }}
 					id="Rect3"
-					transform="translate(187 200)"
-					width="52"
-					height="50"
+					transform="translate(0 170)"
+					width="150"
+					height="150"
 					x="0.5"
 					y="0.5"
 				/>
 				<text
 					id="txt3"
-					transform="translate(208 232)"
-					fontSize="20"
-					fontFamily="SegoeUI, Segoe UI"
+					transform="translate(64 260)"
+					fontSize="40"
 				>
 					3
 				</text>
 				<rect
-					style={{ fill: "#fff", stroke: "#707070", strokeWidth: "1" }}
+					style={{ fill: "#fff", stroke: "#000000", strokeWidth: "1" }}
 					id="Rect4"
-					transform="translate(250 200)"
-					width="52"
-					height="50"
+					transform="translate(175 170)"
+					width="150"
+					height="150"
 					x="0.5"
 					y="0.5"
 				/>
 				<text
 					id="txt4"
-					transform="translate(272 232)"
-					fontSize="20"
-					fontFamily="SegoeUI, Segoe UI"
+					transform="translate(239 260)"
+					fontSize="40"
 				>
 					4
 				</text>
 				<rect
-					style={{ fill: "#fff", stroke: "#707070", strokeWidth: "1" }}
+					style={{ fill: "#fff", stroke: "#000000", strokeWidth: "1" }}
 					id="Rect5"
-					transform="translate(187 261)"
-					width="52"
-					height="50"
+					transform="translate(0 340)"
+					width="150"
+					height="150"
 					x="0.5"
 					y="0.5"
 				/>
 				<text
 					id="txt5"
-					transform="translate(209 293)"
-					fontSize="20"
-					fontFamily="SegoeUI, Segoe UI"
+					transform="translate(64 430)"
+					fontSize="40"
 				>
 					5
 				</text>
 				<rect
-					style={{ fill: "#fff", stroke: "#707070", strokeWidth: "1" }}
+					style={{ fill: "#fff", stroke: "#000000", strokeWidth: "1" }}
 					id="Rect6"
-					transform="translate(250 261)"
-					width="52"
-					height="50"
+					transform="translate(175 340)"
+					width="150"
+					height="150"
 					x="0.5"
 					y="0.5"
 				/>
 				<text
 					id="txt6"
-					transform="translate(272 293)"
-					fontSize="20"
-					fontFamily="SegoeUI, Segoe UI"
+					transform="translate(239 430)"
+					fontSize="40"
 				>
 					6
 				</text>
