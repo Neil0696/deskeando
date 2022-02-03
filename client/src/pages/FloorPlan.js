7bed263b07ea6032a348087c82618f68b5dbbe08
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2022-02-03 00:50:42 +0000

    extract Desk as a component

--- client/src/pages/FloorPlan.js
+++ client/src/pages/FloorPlan.js
@@ -1,5 +1,21 @@
 import React from "react";
 
+const Desk = (props) => {
+	return (
+		<g>
+			<rect
+				style={{ fill: "#fff", stroke: "#000000", strokeWidth: "1" }}
+				width={150}
+				height={150}
+				x={0.5}
+				y={0.5}
+			/>
+			<text fontSize={40} x={64} y={90}>
+				{props.desk}
+			</text>
+		</g>
+	);
+};
 
 const FloorPlan = () => {
 	return (
@@ -18,102 +34,24 @@ const FloorPlan = () => {
 				y="0.5"
 			/>
 			<g id="Group1" transform="translate(262 100)">
-				<rect
-					style={{ fill: "#fff", stroke: "#000000", strokeWidth: "1" }}
-					id="Rect1"
-					transform="translate(0 0)"
-					width="150"
-					height="150"
-					x="0.5"
-					y="0.5"
-				/>
-				<text
-					id="txt1"
-					transform="translate(64 90)"
-					fontSize="40"
-				>
-					1
-				</text>
-				<rect
-					style={{ fill: "#fff", stroke: "#000000", strokeWidth: "1" }}
-					id="Rect2"
-					transform="translate(175 0)"
-					width="150"
-					height="150"
-					x="0.5"
-					y="0.5"
-				/>
-				<text
-					id="txt2"
-					transform="translate(239 90)"
-					fontSize="40"
-				>
-					2
-				</text>
-				<rect
-					style={{ fill: "#fff", stroke: "#000000", strokeWidth: "1" }}
-					id="Rect3"
-					transform="translate(0 170)"
-					width="150"
-					height="150"
-					x="0.5"
-					y="0.5"
-				/>
-				<text
-					id="txt3"
-					transform="translate(64 260)"
-					fontSize="40"
-				>
-					3
-				</text>
-				<rect
-					style={{ fill: "#fff", stroke: "#000000", strokeWidth: "1" }}
-					id="Rect4"
-					transform="translate(175 170)"
-					width="150"
-					height="150"
-					x="0.5"
-					y="0.5"
-				/>
-				<text
-					id="txt4"
-					transform="translate(239 260)"
-					fontSize="40"
-				>
-					4
-				</text>
-				<rect
-					style={{ fill: "#fff", stroke: "#000000", strokeWidth: "1" }}
-					id="Rect5"
-					transform="translate(0 340)"
-					width="150"
-					height="150"
-					x="0.5"
-					y="0.5"
-				/>
-				<text
-					id="txt5"
-					transform="translate(64 430)"
-					fontSize="40"
-				>
-					5
-				</text>
-				<rect
-					style={{ fill: "#fff", stroke: "#000000", strokeWidth: "1" }}
-					id="Rect6"
-					transform="translate(175 340)"
-					width="150"
-					height="150"
-					x="0.5"
-					y="0.5"
-				/>
-				<text
-					id="txt6"
-					transform="translate(239 430)"
-					fontSize="40"
-				>
-					6
-				</text>
+				<g transform="translate(0 0)">
+					<Desk desk="1" />
+				</g>
+				<g transform="translate(175 0)">
+					<Desk desk="2" />
+				</g>
+				<g transform="translate(0 170)">
+					<Desk desk="3" />
+				</g>
+				<g transform="translate(175 170)">
+					<Desk desk="4" />
+				</g>
+				<g transform="translate(0 340)">
+					<Desk desk="5" />
+				</g>
+				<g transform="translate(175 340)">
+					<Desk desk="6" />
+				</g>
 			</g>
 		</svg>
 	);
