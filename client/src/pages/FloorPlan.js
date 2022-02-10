26000429210491fd8abad9b5316e20ad14e28fa5
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2022-02-10 15:31:46 +0000

    update hover desks functionality

--- client/src/pages/FloorPlan.js
+++ client/src/pages/FloorPlan.js
@@ -8,9 +8,9 @@ const Desk = ({ desk, booking }) => {
 	}`;
 	return (
 		<>
-			<g>
+			<g className={deskColor}>
 				<rect
-					className={deskColor}
+					// className={deskColor}
 					width={112}
 					height={160}
 					x={0.5}
@@ -18,11 +18,11 @@ const Desk = ({ desk, booking }) => {
 					rx={15}
 				/>
 			</g>
-			<g transform={`rotate(${desk.r} 56 80)`}>
+			<g className={placeMarkerColor} transform={`rotate(${desk.r} 56 80)`}>
 				<path
 					d="M44.461,47.852a9.858,9.858,0,0,1-10.115-9.57,9.858,9.858,0,0,1,10.115-9.57,9.858,9.858,0,0,1,10.115,9.57,9.858,9.858,0,0,1-10.115,9.57m0-23.926c-8.381,0-15.173,6.427-15.173,14.356s6.792,14.356,15.173,14.356,15.173-6.427,15.173-14.356S52.842,23.926,44.461,23.926M9.058,36.377c0-16.83,16.544-31.592,35.4-31.592s35.4,14.762,35.4,31.592c0,16.533-12.963,36.095-35.4,69.515-22.7-33.846-35.4-52.982-35.4-69.515M44.461,0C23.229,0,4,16.284,4,36.377s17.545,44.072,40.461,78.468c22.916-34.4,40.461-58.38,40.461-78.468S65.7,0,44.461,0"
 					transform="translate(-115 120) rotate(-90)"
-					className={placeMarkerColor}
+					// className={placeMarkerColor}
 				/>
 			</g>
 			<g className="desk-detail" transform="translate(56 90)">
