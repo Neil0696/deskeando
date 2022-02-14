96a8f2c915f24a0fbbd0e68da48cad03fff2c2de
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2022-02-14 18:05:59 +0000

    fix Floor plan functionality

--- client/src/pages/FloorPlan.js
+++ client/src/pages/FloorPlan.js
@@ -2,33 +2,28 @@ import React from "react";
 import "./FloorPlan.css";
 
 const Desk = ({ desk, booking, isSelected }) => {
-	const colour = (element) =>
-		booking
-			? `booked-${element}`
-			: isSelected
-			? `selected-${element}`
-			: `available-${element}`;
+	const deskClassName = booking
+		? `booked`
+		: isSelected
+		? `selected`
+		: `available`;
 
 	return (
-		<>
+		<g className={deskClassName}>
 			<g>
 				<rect
-					className={colour("desk")}
+					className={"desk-rectangle"}
 					width={112}
 					height={160}
 					x={0.5}
 					y={0.5}
-					rx={15}
+					rx={12}
 				/>
 			</g>
-			<g
-				className={colour("place-marker")}
-				transform={`rotate(${desk.r} 56 80)`}
-			>
+			<g className={"place-marker"} transform={`rotate(${desk.r} 56 80)`}>
 				<path
 					d="M44.461,47.852a9.858,9.858,0,0,1-10.115-9.57,9.858,9.858,0,0,1,10.115-9.57,9.858,9.858,0,0,1,10.115,9.57,9.858,9.858,0,0,1-10.115,9.57m0-23.926c-8.381,0-15.173,6.427-15.173,14.356s6.792,14.356,15.173,14.356,15.173-6.427,15.173-14.356S52.842,23.926,44.461,23.926M9.058,36.377c0-16.83,16.544-31.592,35.4-31.592s35.4,14.762,35.4,31.592c0,16.533-12.963,36.095-35.4,69.515-22.7-33.846-35.4-52.982-35.4-69.515M44.461,0C23.229,0,4,16.284,4,36.377s17.545,44.072,40.461,78.468c22.916-34.4,40.461-58.38,40.461-78.468S65.7,0,44.461,0"
 					transform="translate(-115 120) rotate(-90)"
-					// className={placeMarkerColor}
 				/>
 			</g>
 			<g className="desk-detail" transform="translate(56 90)">
@@ -37,7 +32,7 @@ const Desk = ({ desk, booking, isSelected }) => {
 					{booking?.name}
 				</text>
 			</g>
-		</>
+		</g>
 	);
 };
 
@@ -52,15 +47,16 @@ const FloorPlan = ({ desks, bookings, deskId, setDeskId }) => {
 			xmlns="http://www.w3.org/2000/svg"
 			width="390"
 			height="330"
-			viewBox="0 0 650 660"
+			viewBox="0 0 652 660"
 		>
 			<rect
-				style={{ fill: "#fff", stroke: "#000000", strokeWidth: "2" }}
+				className="island"
 				id="Rect7"
 				width="649"
 				height="657"
 				x="0.5"
 				y="0.5"
+				rx="55"
 			/>
 			<g id="Group1" transform="translate(210 85)">
 				{desks.map((desk, i) => (
