484372cd77d5095df472b8dac38045dca30681f5
O'neil Malik <oneilmalik09@gmail.com>
2022-02-17 11:13:52 +0000

    display the error message for desks

--- client/src/pages/ModalBookingScreen.js
+++ client/src/pages/ModalBookingScreen.js
@@ -99,9 +99,9 @@ function ModalBookingScreen({
 			trigger={
 				// <Icon link name="add" size="small" circular inverted color="teal" />
 				<Icon.Group>
-    			<Icon name='map marker alternate' inverted color='orange'/>
-    			<Icon corner name='add' color='orange'/>
-  				</Icon.Group>
+					<Icon name="map marker alternate" inverted color="orange" />
+					<Icon corner name="add" color="orange" />
+				</Icon.Group>
 			}
 		>
 			<Modal.Content>
@@ -135,12 +135,8 @@ function ModalBookingScreen({
 										/>
 									</Form.Field>
 									<Divider inverted />
-									<Form.Field inline>
-										<Popup
-											content={formatBookingDate(bookingDate)}
-											trigger={<Icon name="calendar alternate outline" />}
-										/>
-										<Input
+									<Form.Field>
+										<Form.Input
 											error={
 												dateErrorMessage && {
 													content: dateErrorMessage,
