34d0bd652702a80e93d852d0e63aed5f700334a0
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2022-02-18 22:54:16 +0000

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
 import { Route, Switch } from "react-router-dom";
 
-// import About from "./pages/About";
+import About from "./pages/About";
 import Home from "./pages/Home";
+import Hero from "./pages/Hero";
+import Header from "./pages/Header";
+import Footer from "./pages/Footer";
 
 const App = () => (
-	<Switch>
-		<Route path="/" exact>
-			<Home />
-		</Route>
-		{/* <Route path="/about/this/site">
-			<About />
-		</Route> */}
-	</Switch>
+	<>
+	<Header />
+		<Switch>
+			<Route path="/" exact>
+				<Hero />
+			</Route>
+			<Route path="/home" exact>
+				<Home />
+			</Route>
+			<Route path="/about/this/site">
+				<About />
+			</Route>
+		</Switch>
+		<Footer />
+	</>
 );
 
 export default App;
